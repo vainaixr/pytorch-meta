c754d4de9cce8cf73a6c89b09013e67d204c8f25
Tobias Wuerfl <tobias.wuerfl@fau.de>
2019-04-04 13:11:45 -0400

    Updates test for the past changes and adds a test for the last bugfix

--- torchmeta/tests/TCGATests.py
+++ torchmeta/tests/TCGATests.py
@@ -26,21 +26,21 @@ class TestDatasets(unittest.TestCase):
 
     def setUp(self):
         self.task_id = ('gender', 'BRCA')
-        data_dir = os.path.join('.', 'data')
+        data_dir = os.path.join('C:/Users/Tobias/Documents', 'data')
         if not os.path.exists(data_dir):
             os.makedirs(data_dir)
-        tmds.TCGA(data_dir, download=True)
+        tmds.datasets.TCGA(data_dir, download=True)
         self.data_dir = data_dir
 
     def test_download_meta(self):
         data_dir = os.path.join('.', 'temp')
         os.makedirs(data_dir)
-        tmds.TCGA(data_dir, download=True)
+        tmds.datasets.TCGA(data_dir, download=True)
         shutil.rmtree(data_dir)
 
     def test_tasks(self):
 
-        dataset = tmds.TCGATask.from_id(self.data_dir, self.task_id)
+        dataset = tmds.datasets.tcga.TCGATask.from_id(self.data_dir, self.task_id)
         trainloader = torch.utils.data.DataLoader(dataset=dataset, batch_size=15, shuffle=True)
 
         for data, labels in trainloader:
@@ -48,7 +48,7 @@ class TestDatasets(unittest.TestCase):
                 print('\n', [dataset.categories[label] for label in labels])
 
     def test_meta_TCGA(self):
-        dataset = tmds.TCGA(self.data_dir)
+        dataset = tmds.datasets.TCGA(self.data_dir)
         trainloader = tmds.MetaDataLoader(dataset, batch_size=15, shuffle=True)
 
         for batch in trainloader:
@@ -56,6 +56,13 @@ class TestDatasets(unittest.TestCase):
                 with nostdout():
                     print('\n', dataset.id)
 
+    def test_split(self):
+        dataset = tmds.datasets.TCGA(self.data_dir)
+        counts = [50, 50, 96]
+        train, valid, test = tmds.datasets.split_tcga(dataset, counts)
+        result_counts = [len(train), len(valid), len(test)]
+        result = all([a == b for a, b in zip(counts, result_counts)])
+        self.assertTrue(result)
 
 if __name__ == '__main__':
     unittest.main()
